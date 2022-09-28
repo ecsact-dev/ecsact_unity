b33b5c36fe7b22f079aff76649cd7928916c7463
Ezekiel Warren <zekewarren@gmail.com>
2022-09-28 16:32:07 -0700

    Don't allow ecsact component editing during play mode

--- Editor/DynamicEntityEditor.cs
+++ Editor/DynamicEntityEditor.cs
@@ -10,15 +10,19 @@ namespace Ecsact.Editor {
 	[CustomEditor(typeof(Ecsact.DynamicEntity))]
 	public class DynamicEntityEditor : UnityEditor.Editor {
 		public override void OnInspectorGUI() {
+			EditorGUI.BeginDisabledGroup(Application.isPlaying);
 			DrawDefaultInspector();
+			EditorGUI.EndDisabledGroup();
 
 			if(Application.isPlaying) {
 				var dynamicEntity = (target as DynamicEntity)!;
 
 				EditorGUILayout.LabelField("Runtime Info");
 				EditorGUI.indentLevel += 1;	
-				EditorGUILayout.PrefixLabel("Entity ID");
-				EditorGUILayout.SelectableLabel(dynamicEntity.entityId.ToString());
+				EditorGUILayout.LabelField(
+					"Entity ID",
+					dynamicEntity.entityId.ToString()
+				);
 				EditorGUI.indentLevel -= 1;
 			}
 		}
