567983618e58577f539c37818ddfa217d98a181e
Ezekiel Warren <zekewarren@gmail.com>
2022-09-28 10:32:40 -0700

    Removed some wasm related unnecessary logs

--- EcsactWasm/Runtime/EcsactWasmRuntimeLoader.cs
+++ EcsactWasm/Runtime/EcsactWasmRuntimeLoader.cs
@@ -13,20 +13,10 @@ namespace Ecsact {
 			}
 
 			var defaultRuntime = EcsactRuntime.GetOrLoadDefault();
-			// defaultRuntime.wasm.
-
-			foreach(var assembly in AppDomain.CurrentDomain.GetAssemblies()) {
-				foreach(var type in assembly.GetTypes()) {
-					if(Util.IsSystem(type) || Util.IsAction(type)) {
-						Debug.Log($"System {type.Name} ID is {Util.GetSystemID(type)}");
-					}
-				}
-			}
 
 			foreach(var entry in settings.wasmSystemEntries) {
 				if(string.IsNullOrWhiteSpace(entry.wasmExportName)) continue;
 
-				Debug.Log($"Loading {entry.wasmExportName}");
 				defaultRuntime.wasm.Load(
 					entry.wasmAsset.bytes,
 					entry.systemId,
