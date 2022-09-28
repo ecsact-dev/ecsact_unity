ac86445d049388647677bee442467accae4de7f1
Ezekiel Warren <zekewarren@gmail.com>
2022-09-28 11:05:03 -0700

    formatting

--- Runtime/RegisterScriptPool.cs
+++ Runtime/RegisterScriptPool.cs
@@ -6,50 +6,50 @@ using UnityEngine;
 using Ecsact.UnitySync;
 
 public class RegistryEntitySource : EntityGameObjectPool.EntitySource {
-    EcsactRuntime runtime;
-    private int registryId;
+	EcsactRuntime runtime;
+	private int registryId;
 
-    internal RegistryEntitySource(int registryId, EcsactRuntime runtime) {
-        this.runtime = runtime;
-        this.registryId = registryId;
-    }
+	internal RegistryEntitySource(int registryId, EcsactRuntime runtime) {
+		this.runtime = runtime;
+		this.registryId = registryId;
+	}
 
-    public override object GetComponent(int entityId, int componentId) {
-        return runtime.core.GetComponent(
-            registryId,
-            entityId,
-            componentId
-        );
-    }
+	public override object GetComponent(int entityId, int componentId) {
+		return runtime.core.GetComponent(
+			registryId,
+			entityId,
+			componentId
+		);
+	}
 
-    public override bool HasComponent(int entityId, int componentId) {
-        return runtime.core.HasComponent(
-            registryId,
-            entityId,
-            componentId
-        );
-    }
+	public override bool HasComponent(int entityId, int componentId) {
+		return runtime.core.HasComponent(
+			registryId,
+			entityId,
+			componentId
+		);
+	}
 }
 
 public class RegisterScriptPool : MonoBehaviour {
 
-    void Awake() {
-        var settings = EcsactRuntimeSettings.Get();
-        var monobehaviours = settings.unitySyncScripts;
+	void Awake() {
+		var settings = EcsactRuntimeSettings.Get();
+		var monobehaviours = settings.unitySyncScripts;
 
-        foreach(var monoStr in monobehaviours) {
+		foreach(var monoStr in monobehaviours) {
 
-            var type = Type.GetType(monoStr + ",Assembly-CSharp");
-            if(type == null) {
-                throw new Exception(
-                    "Unity Sync: Monobehaviour " + monoStr + " not found "
-                );
-            } else {
-                UnitySyncMonoBehaviours.RegisterMonoBehaviourType(
-                    type
-                );
-            }
+			var type = Type.GetType(monoStr + ",Assembly-CSharp");
+			if(type == null) {
+				throw new Exception(
+					"Unity Sync: Monobehaviour " + monoStr + " not found "
+				);
+			} else {
+				UnitySyncMonoBehaviours.RegisterMonoBehaviourType(
+					type
+				);
+			}
 
-        }
-    }
+		}
+	}
 }
