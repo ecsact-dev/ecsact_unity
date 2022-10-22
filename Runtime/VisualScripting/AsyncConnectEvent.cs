6ff84812fc6aef82c030234c3264118efa7ed49d
Ezekiel Warren <zekewarren@gmail.com>
2022-10-21 17:48:37 -0700

    formatting

--- Runtime/VisualScripting/AsyncConnectEvent.cs
+++ Runtime/VisualScripting/AsyncConnectEvent.cs
@@ -3,48 +3,48 @@ using UnityEngine;
 using System;
 
 namespace Ecsact.VisualScripting {
-	public class AsyncConnectEventData {
-		public string connectAddress;
-		public Int32 connectPort;
+
+public class AsyncConnectEventData {
+	public string connectAddress;
+	public Int32  connectPort;
+}
+
+[UnitTitle("On Connect")]
+[UnitCategory("Ecsact\\Async")]
+[UnitSurtitle("Ecsact / Async")]
+public class AsyncConnectEvent : EventUnit<AsyncConnectEventData> {
+	public const string eventName = "EcsactAsyncConnectEvent";
+
+	[PortLabel("Connect Address")]
+	[DoNotSerialize]
+	public ValueOutput connectAddressOutput { get; private set; }
+
+	[PortLabel("Connect port")]
+	[DoNotSerialize]
+	public ValueOutput connectPortOutput {
+		get; private set;
 	}
 
-	[UnitTitle("On Connect")]
-	[UnitCategory("Ecsact\\Async")]
-	[UnitSurtitle("Ecsact / Async")]
-	public class AsyncConnectEvent : EventUnit<AsyncConnectEventData> {
-		public const string eventName = "EcsactAsyncConnectEvent";
+	protected override bool register => true;
 
-		[PortLabel("Connect Address")]
-		[DoNotSerialize]
-		public ValueOutput connectAddressOutput { get; private set; }
+	public override EventHook GetHook(GraphReference reference) {
+		return new EventHook(eventName);
+	}
 
-		[PortLabel("Connect port")]
-		[DoNotSerialize]
-		public ValueOutput connectPortOutput { get; private set; }
+	protected override void Definition() {
+		base.Definition();
 
-		protected override bool register => true;
+		connectAddressOutput = ValueOutput<string>(nameof(connectAddressOutput));
+		connectPortOutput = ValueOutput<Int32>(nameof(connectPortOutput));
+	}
 
-		public override EventHook GetHook
-			( GraphReference reference
-			)
-		{
-			return new EventHook(eventName);
-		}
-
-		protected override void Definition() {
-			base.Definition();
-
-			connectAddressOutput = ValueOutput<string>(nameof(connectAddressOutput));
-			connectPortOutput = ValueOutput<Int32>(nameof(connectPortOutput));
-		}
-
-		protected override void AssignArguments
-			( Flow                   flow
-			, AsyncConnectEventData  data
-			)
-		{
-			flow.SetValue(connectAddressOutput, data.connectAddress);
-			flow.SetValue(connectPortOutput, data.connectPort);
-		}
+	protected override void AssignArguments(
+		Flow                  flow,
+		AsyncConnectEventData data
+	) {
+		flow.SetValue(connectAddressOutput, data.connectAddress);
+		flow.SetValue(connectPortOutput, data.connectPort);
 	}
 }
+
+} // namespace Ecsact.VisualScripting
